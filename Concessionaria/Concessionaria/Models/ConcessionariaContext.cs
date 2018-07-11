e29c7f4f3ae85e8d95ea8be4e0d1d7bf1934ffb6
juliano.hubel <juliano_hubel@hotmail.com>
2018-07-11 18:48:10 -0300

    Add Inicializar banco e altera cocessionaria Context

--- Concessionaria/Concessionaria/Models/ConcessionariaContext.cs
+++ Concessionaria/Concessionaria/Models/ConcessionariaContext.cs
@@ -10,12 +10,15 @@ namespace Concessionaria.Models
     public class ConcessionariaContext : DbContext
     {
 
-       // public ConcessionariaContext() : base("name=ConcessionariaContext")
+        // public ConcessionariaContext() : base("name=ConcessionariaContext")
         //{
-            
-          //  Database.Connection.ConnectionString = @"data source=HUBEL\SQLEXPRESS; initial catalog=Concessionarias; User Id=sa; Password=sa;";
+
+        //  Database.Connection.ConnectionString = @"data source=HUBEL\SQLEXPRESS; initial catalog=Concessionarias; User Id=sa; Password=sa;";
         //}
 
+        public ConcessionariaContext() : base("ConcessionariaContext") {  }
+        
+        
         public DbSet<Fabricante> Fabricante { get; set; }
         public DbSet<Carro> Carro { get; set; }        
 
---
+++ Concessionaria/Concessionaria/Models/IncializarBanco.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;   
+
+namespace Concessionaria.Models
+{
+    public class IncializarBanco : DropCreateDatabaseAlways<ConcessionariaContext>
+    {
+        protected override void Seed(ConcessionariaContext context)
+        {
+            //Criar dados 
+            new List<Fabricante>
+            {
+                new Fabricante
+                {
+                    Nome = "Chevrolet",
+                    PaisOrigem= "Alemanha",
+                    Carros  =  new List<Carro>
+                    {
+                        new Carro
+                        {
+                            Nome = "Chevette",
+
+                        }
+                    }
+
+                }
+            }.ForEach(f => context.Fabricante.Add(f));         
+            
+            base.Seed(context);
+        }
+    }
+}
